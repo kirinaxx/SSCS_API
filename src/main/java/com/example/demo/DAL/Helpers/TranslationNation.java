64e603b163dfaf856e25bc50b69da34ba07cd8cd
Ethan Coles <ethanjcoles@gmail.com>
2024-05-03 13:16:39 -0700

    made the translation nation fulfill singleton design pattern

--- src/main/java/com/example/demo/DAL/Helpers/TranslationNation.java
+++ src/main/java/com/example/demo/DAL/Helpers/TranslationNation.java
@@ -3,18 +3,31 @@ import com.deepl.api.*;
 
 
 public class TranslationNation {
-    private static final Translator translator = new Translator("17dc983f-22da-4603-952f-1c3476aadeaf:fx");
+    private static final SingletonTranslator translator = SingletonTranslator.singletonTranslator;
 
     //target is language you want input translated into
     //target must be string ISO 639-1 language code, for example "de" for german, "en-us" for english
     public static String translate(String input, String target) {
+        return translator.translate(input, target);
+    }
+}
+
+class SingletonTranslator {
+    private Translator translator;
+
+    private SingletonTranslator() {
+        translator = new Translator("17dc983f-22da-4603-952f-1c3476aadeaf:fx");
+    }
+
+    public String translate(String input, String target) {
         String output = "";
         try {
             output = translator.translateText(input, null, target).getText();
         } catch (Exception e) {
             output = "error in translating: " + e.getMessage();
         }
-        return (output);
+        return output;
     }
 
+    public static final SingletonTranslator singletonTranslator = new SingletonTranslator();
 }
